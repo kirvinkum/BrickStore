d018878f1ec4863eaf308cd2e2f2a4cdd40dcdc4
vkumar <vinkum099@gmail.com>
2022-10-24 17:42:51 +0100

    Initial commit

---
+++ src/main/java/com/example/brickstore/BrickStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.brickstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brickstore/BrickStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brickstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
